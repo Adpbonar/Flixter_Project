35804a27f51306900ee088fd91e231e7157f3112
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-07-14 15:03:41 +0000

    making sections draggable

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -22,12 +22,14 @@ class Instructor::LessonsController < ApplicationController
   def required_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       return render plain: 'Unauthorized', status: :unauthorized
     end
-  end 
+  end
 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course
+  before_action :required_authorized_for_current_lesson, only: [:update]
 
   def new
     @section = Section.new
@@ -11,6 +12,10 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    reder plain: 'updated!'
+  end
 
   private
 
@@ -21,12 +26,14 @@ class Instructor::SectionsController < ApplicationController
   end
 
   helper_method :current_course
-
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
-
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
