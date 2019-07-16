b7ffc6ad20ee4d4dd4b6061b9bd958e8a01e88cf
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-07-16 19:40:15 +0000

    Production troubleshooting

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,7 +5,7 @@ class Course < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	validates :title, presence: true, length: { minimum: 4 }
 	validates :description, presence: true, length: { minimum: 4 }
-	validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+	validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
 	def free?
 		cost.zero?
